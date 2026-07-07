10c4951b4de413b2f2b5eb0af381a32d891cd9c5
Ashkan <imashkannazari@gmail.com>
2026-07-07 23:12:42 +0330

    feat: integrate website template and static assets

--- myweb/settings.py
+++ myweb/settings.py
@@ -115,6 +115,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'statics/'
-STATICFILES_DIRS = [BASE_DIR / "statics"]
-    
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
